c0166f3acc597e18a3192580f1d2666f3c46ec3c
annisa femiya <annisafemiya@gmail.com>
2022-06-17 21:49:58 +0700

    api user get

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,34 +3,54 @@ const setResponse = require("../helper/response.helper");
 
 
 class UserControllers {
-  
-static async update(req, res,next) {
+  static async getById(req, res, next) {
     try {
-     const user = await User.findOne({
+      const user = await User.findOne({
         where: {
-         id: req.user.id,
-       },
-     })
+          id: req.user.id
+        }
+      })
 
-     if (!user) {
-       throw {
-         status: 404,
-         message: 'User Not Found'
-       }
-     } else {
-       req.body.verified= true
-       const updatedUser= await User.update(req.body, {
-         where: {
-           id: req.user.id
-         },returning:true
-       })
-       console.log(updatedUser,req.body,req.user)
-       const response = setResponse('success', updatedUser[1][0], null)
-       res.status(200).json(response)
-     }
-   } catch (err) {
-     next(err)
-   }   
- }
+      if (!user) {
+        throw {
+          status: 404,
+          message: 'User not found'
+        }
+      } 
+      const response=setResponse("success",user,null)
+        res.status(200). json(response)
+
+    } catch (err) {
+      next(err)
+    }
+  }
+  static async update(req, res, next) {
+    try {
+      const user = await User.findOne({
+        where: {
+          id: req.user.id,
+        },
+      })
+
+      if (!user) {
+        throw {
+          status: 404,
+          message: 'User Not Found'
+        }
+      } else {
+        req.body.verified = true
+        const updatedUser = await User.update(req.body, {
+          where: {
+            id: req.user.id
+          }, returning: true
+        })
+        console.log(updatedUser, req.body, req.user)
+        const response = setResponse('success', updatedUser[1][0], null)
+        res.status(200).json(response)
+      }
+    } catch (err) {
+      next(err)
+    }
+  }
 }
- module.exports = UserControllers
+module.exports = UserControllers
--- routes/user.route.js
+++ routes/user.route.js
@@ -4,7 +4,6 @@ const auth = require("../middlewares/auth");
 // const validate = require("../validation/validator");
 // const UserRule = require("../validation/user.rule");
 const router = require("express").Router();
-
 router.put("/update",[auth], UserControllers .update);
 
 module.exports = router;
